efb57a25bbf10190c2e2320808ed62b5c8a23fdd
AntonioLabinjan <antoniolabinjan5@gmail.com>
2024-10-22 09:53:01 +0200

    Commit2

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ const express = require("express")
 let app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.sendFile(__dirname + "/public/index.html");
+    //public\about.html
+  });
+
+app.get("/about", (req, res) => {
+    res.sendFile(__dirname + "/public/about.html");
+})
+
+app.get("/users", (req, res) => {
+    const users = [
+        {id: 1, ime: "Pino", prezime: "NekoPrezime" } ,
+        {id: 2, ime: "Franko", prezime: "IstoPrezime"},
+        {id: 3, ime: "Mario", prezime: "Battifiaca"}
+
+
+    ]
+    res.json(users);
 })
 
 const port = 3000;
